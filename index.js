b2e51e161f56a6ce4598c52dac19baa90226478f
ills5771 <ilyayelizarov1@gmail.com>
2019-03-22 13:34:18 -0700

    added .env file

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+require("dotenv").config();
+
 const server = require("./server.js");
 
-server.listen(5000, () => {
-  console.log("\n*** Server Running on http://localhost:5000 ***\n");
+const port = process.env.PORT || 4000;
+
+const greeting = process.env.GREETING;
+
+server.listen(port, () => {
+  console.log(`\n* ${greeting} http://localhost:${port}*\n`);
 });
